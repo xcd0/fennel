e84d5893529e24114e3724c7d1f3241190a0fc0e
xcd0 <xcd@xcd0.com>
2020-02-26 00:43:19 +0900

    分けた

---
+++ gbr/zip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv fan-F_Cu.gbr      fan.GTL
+mv fan-F_SilkS.gbr   fan.GTO
+mv fan-F_Mask.gbr    fan.GTS
+mv fan-B_Cu.gbr      fan.GBL
+mv fan-B_SilkS.gbr   fan.GBO
+mv fan-B_Mask.gbr    fan.GBS
+mv fan-Edge_Cuts.gbr fan.GML
+mv fan-PTH.drl       fan.TXT
+mv fan-NPTH.drl      fan-NPTH.TXT
+
